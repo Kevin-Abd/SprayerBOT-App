00a714da1aaca1552006592aefb48861aa5b225a
habedi77 <hossein.abedi1998@gmail.com>
2022-10-03 19:54:48 -0500

    remove phidget debug logs

--- src/phidgetqml.cpp
+++ src/phidgetqml.cpp
@@ -2,23 +2,23 @@
 
 PhidgetQml::PhidgetQml(QObject *parent) : QObject(parent)
 {
-    qDebug("PhidgetQml Initilizing");
+    // qDebug("PhidgetQml Initilizing");
     phidget = new PhidgetInterface();
-    qDebug("PhidgetQml setting handlers");
+    // qDebug("PhidgetQml setting handlers");
     phidget->setHandlers(onAttach, onDetach, NULL);
-    qDebug("PhidgetQml openning");
+    // qDebug("PhidgetQml openning");
     phidget ->opne();
-    qDebug("PhidgetQml done");
+    // qDebug("PhidgetQml done");
 }
 
 
 Q_INVOKABLE bool PhidgetQml::activate() {
-    qDebug("PhidgetQml activate invoked");
+    // qDebug("PhidgetQml activate invoked");
     return phidget->activate();
 }
 
 Q_INVOKABLE bool PhidgetQml::deactivate() {
-    qDebug("PhidgetQml deactivate invoked");
+    // qDebug("PhidgetQml deactivate invoked");
     return phidget->deactivate();
 }
 
@@ -28,12 +28,12 @@ Q_INVOKABLE int PhidgetQml::getState() {
 
 
 void CCONV PhidgetQml::onAttach(PhidgetHandle ch, void * ctx) {
-    qInfo("PhidgetQml Attached");
+    // qInfo("PhidgetQml Attached");
     // TODO call attached
 }
 
 void CCONV PhidgetQml::onDetach(PhidgetHandle ch, void * ctx) {
-    qInfo("PhidgetQml Deatached");
+    // qInfo("PhidgetQml Deatached");
     // TODO call deattached
 }
 
